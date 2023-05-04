722eca7a4b2d3d872193907b5f6227f297782c7d
Yonin408 <justin12689@gmail.com>
2023-05-04 01:24:44 -0700

    inital commit

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Stores < ApplicationRecord
+end
+
+CREATE TABLE Burnaby (
+id INTEGER,
+annual_revenue INTEGER,
+type STRING
+);
+
+CREATE TABLE Richmond (
+id INTEGER,
+annual_revenue INTEGER,
+type STRING
+);
+
+CREATE TABLE Gastown (
+id INTEGER,
+annual_revenue INTEGER,
+type STRING
+);
+
+puts Stores
